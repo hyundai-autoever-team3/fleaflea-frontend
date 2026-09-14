220f56c74a692fc9e2ca4219e44a0c6239f5c425
방연지 <ktths1230@gmail.com>
2026-09-14 11:30:54 +0900

    feat: msw 목업 서버 설정

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,18 @@ import { AppProviders } from './app/provider'
 import './app/styles/index.css'
 import { router } from './app/router/router'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AppProviders>
-      <RouterProvider router={router} />
-    </AppProviders>
-  </StrictMode>,
-)
+async function enableMocking() {
+  if (!import.meta.env.DEV) return
+  const { worker } = await import('./mocks/browser')
+  return worker.start({ onUnhandledRequest: 'bypass' })
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <AppProviders>
+        <RouterProvider router={router} />
+      </AppProviders>
+    </StrictMode>,
+  )
+})
